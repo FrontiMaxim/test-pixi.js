229d0387b3d22fec67a9563ff608655c4c589051
Максим Расторгуев <frontimax@yandex.ru>
2022-06-10 10:59:24 +0400

    created a character to walk on the browser window

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+import {Unit} from "./unit.js";
+
+let Application = PIXI.Application;
+let loader = PIXI.loader;
+let resources = PIXI.loader.resources;
+let AnimatedSprite = PIXI.AnimatedSprite
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const option = {
+    width,
+    height
+};
+
+// встраиваем приложение
+const application = new Application(option);
+application.renderer.backgroundColor = 0xffffff;
+document.body.appendChild(application.view);
+
+const atlasNeanderthal = [
+    {name:"baseLeft", url:"../effects/neanderthalBaseLeft.json"},
+    {name:"baseRight", url:"../effects/neanderthalBaseRight.json"},
+    {name:"goLeft", url:"../effects/neanderthalGoLeft.json"},
+    {name:"goRight", url:"../effects/neanderthalGoRight.json"}
+];
+
+
+const neanderthal = new Unit(atlasNeanderthal, loader, resources, AnimatedSprite, application);
+
+document.addEventListener('keydown', (e) => {
+    neanderthal.changeState(e.key, application);
+});
+
+document.addEventListener('keyup', () => {
+    neanderthal.changeState('base', application);
+})
+
+
+
+
+
+
---
+++ scripts/unit.js
@@ -0,0 +1,80 @@
+export class Unit {
+
+    #state = {
+        x: 0,
+        y: 0,
+        sprite: null,
+        endKeyButton: 'base'
+    };
+
+    #sprites = {}; // спрайты юнита
+
+    constructor(atlasTextures, loader, resource, AnimatedSprite, application) {
+        this.changeState = this.changeState.bind(this);
+        this.changeOrintation = this.changeOrintation.bind(this);
+
+        loader.add(atlasTextures).load(() => {
+
+            const arrResource = Object.values(resource).filter(item => item.extension !== 'png' );
+
+            for(let item of arrResource) {
+                const masterTextures = item.textures;
+                const textures = Object.values(masterTextures);
+                this.#sprites[item.name] = new AnimatedSprite(textures);
+                this.#sprites[item.name].animationSpeed = 0.2;
+            }
+
+            this.#state.sprite = this.#sprites['baseLeft'];
+            application.stage.addChild(this.#state.sprite);
+            this.#state.sprite.play();
+        });
+    }
+
+    // действия
+    #actions = {
+        'a': {nameSprite: 'goLeft', x: -10, y: 0 },
+        'd': {nameSprite: 'goRight', x: 10, y: 0 },
+        'w': {nameSprite: 'goLeft', x: 0, y: -10 },
+        's': {nameSprite: 'goLeft', x: 0, y: 10 },
+        'base': {nameSprite: 'baseLeft', x: 0, y: 0 }
+    }
+
+    // смена ориентации в пространстве
+    changeOrintation(keyButton) {
+        if(keyButton === 'a') {
+            this.#actions['w'].nameSprite = 'goLeft';
+            this.#actions['s'].nameSprite = 'goLeft';
+            this.#actions['base'].nameSprite = 'baseLeft';
+        } else if (keyButton === 'd') {
+            this.#actions['w'].nameSprite = 'goRight';
+            this.#actions['s'].nameSprite = 'goRight';
+            this.#actions['base'].nameSprite = 'baseRight';
+        }
+
+    }
+
+    // изменение состояния юнита
+    changeState(keyButton, application) {
+
+        if (['a', 'd'].includes(keyButton)) {
+            this.changeOrintation(keyButton);
+        }
+
+        const action = this.#actions[keyButton];
+
+        if (keyButton !== this.#state.endKeyButton) {
+
+            application.stage.removeChild(this.#state.sprite);
+            this.#state.sprite = this.#sprites[action.nameSprite];
+            application.stage.addChild(this.#state.sprite);
+
+            this.#state.sprite.play();
+
+            this.#state.endKeyButton = keyButton;
+        }
+        this.#state.x += action.x;
+        this.#state.y += action.y;
+        this.#state.sprite.position.set(this.#state.x, this.#state.y);
+    }
+
+}
